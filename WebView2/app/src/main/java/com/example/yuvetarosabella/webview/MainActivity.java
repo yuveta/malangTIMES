7bb0ab98ddbae6b7ef9d09d0a0b6dc6ba5fd6b55
yuveta <yuvetarosabella23@gmail.com>
2019-07-17 14:25:01 +0700

    okesip

---
+++ WebView2/app/src/main/java/com/example/yuvetarosabella/webview/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.yuvetarosabella.webview;
+
+import android.app.Activity;
+
+public class MainActivity extends Activity {
+}
---
+++ WebView2/app/src/main/java/com/example/yuvetarosabella/webview2/MainActivity.java
@@ -0,0 +1,73 @@
+package com.example.yuvetarosabella.webview2;
+
+import android.annotation.TargetApi;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.RequiresApi;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        webView = (WebView) findViewById(R.id.webView);
+        webView.getSettings().setLoadsImagesAutomatically(true);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setDomStorageEnabled(true);
+        webView.getSettings().setAppCacheEnabled(true);
+        webView.getSettings().setLoadsImagesAutomatically(true);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            webView.getSettings().setMixedContentMode(WebSettings.MIXED_CONTENT_COMPATIBILITY_MODE);
+        }
+        webView.clearCache(false);
+        //webView.getSettings().setAllowUniversalAccessFromFileURLs(true);
+
+        // Tiga baris di bawah ini agar laman yang dimuat dapat
+        // melakukan zoom.
+        webView.getSettings().setSupportZoom(true);
+        webView.getSettings().setBuiltInZoomControls(true);
+        webView.getSettings().setDisplayZoomControls(false);
+
+        // Baris di bawah untuk menambahkan scrollbar di dalam WebView-nya
+        webView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+        //webView.setWebViewClient(new WebViewClient());
+
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://malangtimes.com");
+    }
+
+
+    private class MyBrowser extends WebViewClient
+    {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()){
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+}
+
